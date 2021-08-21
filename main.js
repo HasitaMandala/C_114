af3762c1934dc714997f9c0ad72297346b5220b1
HasitaMandala <74747774+HasitaMandala@users.noreply.github.com>
2021-08-20 19:45:05 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -3,47 +3,14 @@ function preload(){
 }
 
 function setup(){
-    canvas = createCanvas(640, 480);
-    canvas.position(110, 250);
-    video = createCapture(VIDEO);
-    video.hide();
+    canvas = createCanvas(300,300);
+    canvas.center();
 }
 
 function draw(){
-    image(video,230,150,220,200);
-
-    fill(0, 128, 210)
-    stroke(0, 128, 0)
-    rect(-30, 40, 460, 20, 20)
-
-    fill(100, 0, 210)
-    stroke(255, 0, 0)
-    circle(50, 50, 80)
-
-    fill(0, 128, 210)
-    stroke(0, 128, 0)
-    rect(90, 40, 460, 20, 20)
-
-    fill(100, 0, 210)
-    stroke(255, 0, 0)
-    circle(590, 50, 80)
-
-    fill(0, 128, 210)
-    stroke(0, 128, 0)
-    rect(620, 40, 460, 20,)
-
-    fill(0, 128, 210)
-    stroke(0, 128, 0)
-    rect(90, 340, 460, 20, 20)
-
-    fill(0, 128, 210)
-    stroke(0, 128, 0)
-    rect(-10, 400, 660, 500, 20)
-
 
 }
 
 function take_snapshot(){
-    save('Hasita.png');
+    save('myfilterimage.png');
 }
-
